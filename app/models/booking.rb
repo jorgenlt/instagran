7b25a23676c909f3ff5f854e7e764c9c0f53793f
Alex Drew <alexander.william.drew@gmail.com>
2022-11-14 17:04:09 +0800

    added validations and assoc

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :grandma
   belongs_to :user
+  validates :start_time, :end_time, :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+  validates :address, presence: true
+  has_many :grandmas
+  has_many :bookings
 end
