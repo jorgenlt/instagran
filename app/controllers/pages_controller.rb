161bbd8c515b50c5daba23e82e3bbcf16c119e5e
Alex Drew <alexander.william.drew@gmail.com>
2022-11-15 15:47:38 +0800

    created HTML/CSS card and form components

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+  def components_alex
+    @grandma = Grandma.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   resources :bookings, only: [:index]
 
   resources :users, only: [:new, :create, :destroy]
+
+  get '/components_alex' => 'pages#components_alex'
 end
