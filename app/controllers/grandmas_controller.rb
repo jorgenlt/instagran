cb94eceafd3661e0017c05e0cf2d943b6876334e
Joergen Tjoennteig <jorgenlt@outlook.com>
2022-11-16 12:18:57 +0800

    feat: create grandma

--- app/controllers/grandmas_controller.rb
+++ app/controllers/grandmas_controller.rb
@@ -6,11 +6,32 @@ class GrandmasController < ApplicationController
   end
 
   def new
+    @grandma = Grandma.new
   end
 
   def create
+    @grandma = Grandma.new(grandma_params)
+
+
+    if @grandma.save
+      # redirect_to grandma_path(@grandma.id)
+      redirect_to root_path
+    else
+      flash.alert = "Error! Grandma was not created."
+    end
   end
 
   def destroy
+  end
+
+
+
+
+
+
+  private
+
+  def grandma_params
+    params.require(:grandma).permit(:first_name, :last_name, :address, :description)
   end
 end
