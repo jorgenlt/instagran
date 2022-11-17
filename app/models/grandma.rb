8b3a166d521c7fd96f72a2d1c72601095a2f944c
Alexander Neukomm <alexander.neukomm@gmail.com>
2022-11-17 04:35:54 +0100

    added search feature

--- app/models/grandma.rb
+++ app/models/grandma.rb
@@ -3,4 +3,10 @@ class Grandma < ApplicationRecord
   validates :description, length: { minimum: 10 }
   has_many :bookings
   belongs_to :user
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+  against: [ :first_name, :last_name, :description ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
